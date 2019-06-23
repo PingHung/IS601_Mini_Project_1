2697c093c6ebe829d82eb648d953eee5b07800ab
ping <ph49@njit.edu>
2019-06-23 18:55:42 -0400

    Task: create classes for csv, html, system, and main

--- public/index.php
+++ public/index.php
@@ -1,4 +1,36 @@
 <?php
-echo "hello world <br> ";
-echo "heroku";
+
+
+main::start();
+
+class main
+{
+    static public function start()
+    {
+        $records = csv::getrecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
+
+
+class csv
+{
+    static public function getRecords() {}
+}
+class html
+{
+    static public function generateTable($records)
+    {
+        $table = 'test';
+        return $table;
+    }
+}
+class system
+{
+    static public function printPage($page)
+    {
+        echo $page;
+    }
+}
 ?>
