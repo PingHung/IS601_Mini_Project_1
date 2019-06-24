281ca76fe07c3b81c95e5d8c0608b93d4778ca86
ping <ph49@njit.edu>
2019-06-24 13:04:30 -0400

    Feature: Create Class record and recordFacotry for record an array of file

--- public/index.php
+++ public/index.php
@@ -6,14 +6,15 @@ fclose($file);*/
 main::start("example.csv");
 
 class main {
-    static public function star($filename){
-
+    static public function start($filename){
         $records = csv::getRecords($filename);
-        print_r($records);
+        $record = recordFactory::create();
+        print_r($record);
     }
 }
 
 class csv {
+
     static public function getRecords($filename){
         $file = fopen($filename, "r");
         while(! feof($file))
@@ -21,12 +22,23 @@ class csv {
             $record = fgetcsv($file);
             $records[] = $record;
         }
-
         fclose($file);
         return $records;
     }
 }
 
+class record {
+
+}
+
+class recordFactory {
+    static public function create(Array $array = null) {
+        $record = new record();
+        return $record;
+
+    }
+
+}
 /*
 main::start();
 
