8623bc069ff8a3ae30a3ea2c0915afb05ef4f2ba
ping <ph49@njit.edu>
2019-06-24 20:34:07 -0400

    Task: Clear out not used codes

--- public/index.php
+++ public/index.php
@@ -102,45 +102,4 @@ class system
     }
 }
 
-/*
-main::start();
-
-class main
-{
-    static public function start()
-    {
-        $test = 'test';
-
-        $records = csv::getrecords();
-        $table = html::generateTable($records);
-        system::printPage($table);
-    }
-}
-
-class csv
-{
-    static public function getRecords()
-    {
-        $records[] = $test;
-
-        return $records;
-    }
-}
-class html
-{
-    static public function generateTable($records)
-    {
-        $table = $records;
-        return $table;
-    }
-}
-class system
-{
-    static public function printPage($page)
-    {
-        echo $page;
-    }
-}
-*/
-
 ?>
